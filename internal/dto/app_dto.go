cf76ad5a4dd10b3abeab504a2ac7eb956ade218f
HaierKeys <haierkeys@gmail.com>
2026-02-23 04:33:56 +0800

    版本检测

--- internal/dto/app_dto.go
+++ internal/dto/app_dto.go
@@ -5,10 +5,11 @@ package dto
 // VersionDTO version information for API response
 // VersionDTO 版本信息 API 响应对象
 type VersionDTO struct {
-	Version        string `json:"version"`        // Current version // 当前版本
-	GitTag         string `json:"gitTag"`         // Git tag // Git 标签
-	BuildTime      string `json:"buildTime"`      // Build time // 构建时间
-	VersionIsNew   bool   `json:"versionIsNew"`   // Is there a new version // 是否有新版本
-	VersionNewName string `json:"versionNewName"` // New version name // 新版本名称
-	VersionNewLink string `json:"versionNewLink"` // New version download link // 新版本下载链接
+	Version             string `json:"version"`             // Current version // 当前版本
+	GitTag              string `json:"gitTag"`              // Git tag // Git 标签
+	BuildTime           string `json:"buildTime"`           // Build time // 构建时间
+	VersionIsNew        bool   `json:"versionIsNew"`        // Is there a new version // 是否有新版本
+	VersionNewName      string `json:"versionNewName"`      // New version name // 新版本名称
+	VersionNewLink      string `json:"versionNewLink"`      // New version download link // 新版本下载链接
+	VersionNewChangelog string `json:"versionNewChangelog"` // New version changelog // 新版本更新日志
 }
--- internal/task/task_check_version.go
+++ internal/task/task_check_version.go
@@ -14,16 +14,31 @@ import (
 )
 
 const (
-	ServiceVersionURL = "https://img.shields.io/github/v/release/haierkeys/fast-note-sync-service.json"
-	PluginVersionURL  = "https://img.shields.io/github/v/tag/haierkeys/obsidian-fast-note-sync.json"
+	GitHubCheckURL    = "https://api.github.com"
+	ServiceRepoPath   = "haierkeys/fast-note-sync-service"
+	ServiceRepoURL    = "https://github.com/" + ServiceRepoPath
+	PluginRepoPath    = "haierkeys/obsidian-fast-note-sync"
+	PluginRepoURL     = "https://github.com/" + PluginRepoPath
+	ServiceVersionURL = "https://img.shields.io/github/v/release/" + ServiceRepoPath + ".json"
+	PluginVersionURL  = "https://img.shields.io/github/v/tag/" + PluginRepoPath + ".json"
+
+	CNBServiceReleaseURL = "https://api.cnb.cool/" + ServiceRepoPath + "/-/releases"
+	CNBPluginReleaseURL  = "https://api.cnb.cool/" + PluginRepoPath + "/-/releases"
+	CNBServiceURL        = "https://cnb.cool/" + ServiceRepoPath
+	CNBPluginURL         = "https://cnb.cool/" + PluginRepoPath
 )
+
+type CNBRelease struct {
+	TagName string `json:"tag_name"`
+}
 
 type ShieldsJSON struct {
 	Message string `json:"message"`
 }
 
 type CheckVersionTask struct {
-	app *app.App
+	app      *app.App
+	isGitHub bool
 }
 
 func init() {
@@ -39,14 +54,39 @@ func (t *CheckVersionTask) Name() string {
 }
 
 func (t *CheckVersionTask) Run(ctx context.Context) error {
-	serviceLatest, err := t.fetchVersion(ServiceVersionURL)
-	if err != nil {
-		return err
-	}
+	t.isGitHub = t.checkGitHub()
 
-	pluginLatest, err := t.fetchVersion(PluginVersionURL)
-	if err != nil {
-		return err
+	var serviceLatest, pluginLatest string
+	var serviceLink, pluginLink string
+	var serviceChangelog, pluginChangelog string
+	var err error
+
+	if t.isGitHub {
+		serviceLatest, err = t.fetchVersion(ServiceVersionURL)
+		if err != nil {
+			return err
+		}
+		pluginLatest, err = t.fetchVersion(PluginVersionURL)
+		if err != nil {
+			return err
+		}
+		serviceLink = ServiceRepoURL + "/releases/tag/" + serviceLatest
+		pluginLink = PluginRepoURL + "/releases/tag/" + pluginLatest
+		serviceChangelog = ServiceRepoURL + "/releases/download/" + serviceLatest + "/changelog.txt"
+		pluginChangelog = PluginRepoURL + "/releases/download/" + pluginLatest + "/changelog.txt"
+	} else {
+		serviceLatest, err = t.fetchCNBVersion(CNBServiceReleaseURL)
+		if err != nil {
+			return err
+		}
+		pluginLatest, err = t.fetchCNBVersion(CNBPluginReleaseURL)
+		if err != nil {
+			return err
+		}
+		serviceLink = CNBServiceURL + "/-/releases/tag/" + serviceLatest
+		pluginLink = CNBPluginURL + "/-/releases/tag/" + pluginLatest
+		serviceChangelog = CNBServiceURL + "/-/releases/download/" + serviceLatest + "/changelog.txt"
+		pluginChangelog = CNBPluginURL + "/-/releases/download/" + pluginLatest + "/changelog.txt"
 	}
 
 	currentServiceVersion := t.app.Version().Version
@@ -63,9 +103,14 @@ func (t *CheckVersionTask) Run(ctx context.Context) error {
 	}
 
 	info := pkgapp.CheckVersionInfo{
-		VersionNewName:       serviceLatest,
-		VersionIsNew:         semver.Compare(serviceLatest, currentServiceVersion) > 0,
-		PluginVersionNewName: pluginLatest,
+		GithubAvailable:           t.isGitHub,
+		VersionNewName:            serviceLatest,
+		VersionIsNew:              semver.Compare(serviceLatest, currentServiceVersion) > 0,
+		VersionNewLink:            serviceLink,
+		VersionNewChangelog:       serviceChangelog,
+		PluginVersionNewName:      pluginLatest,
+		PluginVersionNewLink:      pluginLink,
+		PluginVersionNewChangelog: pluginChangelog,
 		// 这里无法判断 PluginVersionIsNew，因为没有具体的客户端版本，
 		// 但我们还是更新最新的版本名，具体的比较逻辑可以在 App.CheckVersion 中根据传入的参数进行。
 	}
@@ -94,6 +139,42 @@ func (t *CheckVersionTask) fetchVersion(url string) (string, error) {
 	}
 
 	return sj.Message, nil
+}
+
+func (t *CheckVersionTask) fetchCNBVersion(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var releases []CNBRelease
+	if err := json.Unmarshal(body, &releases); err != nil {
+		return "", err
+	}
+
+	if len(releases) == 0 {
+		return "", nil
+	}
+
+	return releases[0].TagName, nil
+}
+
+func (t *CheckVersionTask) checkGitHub() bool {
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	resp, err := client.Head(GitHubCheckURL)
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+	return resp.StatusCode == http.StatusOK
 }
 
 func (t *CheckVersionTask) LoopInterval() time.Duration {
