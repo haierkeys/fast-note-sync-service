4c36d4e6f6361674c7b575830df7270a6d34df09
HaierKeys <haierkeys@gmail.com>
2026-02-25 02:01:24 +0800

    新增 服务端 在线升级 功能.. 修正 语言包无法切换/切换后语言包失效的问题

--- internal/app/restart_linux.go
+++ internal/app/restart_linux.go
@@ -1,13 +1,3 @@
-//go:build !windows
+//go:build ignore
 
 package app
-
-import (
-	"syscall"
-)
-
-// RestartProcess restarts the current process using syscall.Exec
-// RestartProcess 使用 syscall.Exec 重启当前进程
-func RestartProcess(argv0 string, args []string, env []string) error {
-	return syscall.Exec(argv0, args, env)
-}
